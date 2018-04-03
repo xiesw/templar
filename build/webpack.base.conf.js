87a181c7998e2d739579c6fa56f156722715771a
pain.xie <pain.xie@wolaidai.com>
2018-04-03 16:28:30 +0800

    v1.0.0 webpack配置

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const APP_PATH = path.resolve(__dirname, '../src');
+const PUBLIC_PATH = path.resolve(__dirname, '../public');
+const DIST_PATH = path.resolve(__dirname, '../dist');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    framework: ['react', 'react-dom']
+  },
+  output: {
+    filename: "js/bundle.js",
+    path: DIST_PATH
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: "babel-loader",
+        include: APP_PATH
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+        include: APP_PATH
+      },
+      {
+        test: /\.(png|jpg|gif|svg|ico|json)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+        include: [
+          APP_PATH,
+          PUBLIC_PATH
+        ]
+      },
+    ]
+  }
+};
