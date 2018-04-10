775c9dadf4cf95c4aa45a19865b484ce2fcbd101
pain.xie <pain.xie@wolaidai.com>
2018-04-10 21:13:39 +0800

    v1.0.0 更换第4批盒子方

--- src/net/data.js
+++ src/net/data.js
@@ -12,29 +12,31 @@ export const data = [
     "code": "weilixin"
   },
   {
-    "joincount": "1348734",
-    "imagepath": "yangcongxinyong.png",
-    "name": "洋葱信用",
-    "recommendDesc": "无抵押，高通过",
-    "applyUrl": "http://channel.yangcongjietiao.com/channel/view/register.html?channel=jieyoubang",
-    "code": "yangcongxinyong"
+    "joincount": "690321",
+    "imagepath": "woaika.png",
+    "name": "我爱卡",
+    "recommendDesc": "万元额度，7天免息",
+    "applyUrl": "http://m.51credit.com/promote/wt20170918.html?tf=wt34",
+    "code": "woaika"
+  },
+
+  {
+    "joincount": "569832",
+    "imagepath": "kanglexiang.png",
+    "name": "康乐享",
+    "recommendDesc": "极速认证，最高两万",
+    "applyUrl": "http://lxbk.jjkswl.com/#/proxyReg?inviteCode=A100447",
+    "code": "kanglexiang"
   },
   {
-    "joincount": "893234",
-    "imagepath": "jiyonghebao.png",
-    "name": "急用荷包",
-    "recommendDesc": "芝麻分580，无逾期秒下",
-    "applyUrl": "http://business.haodaihome.com/haodai/loanCompanyHomePage?configCode=da6af473-ec6c-4dc0-82ac-7ea7d8d09cd9&channelCode=jieyoubang&agentId=54564",
-    "code": "jiyonghebao"
+    "joincount": "1579342",
+    "imagepath": "juyian.png",
+    "name": "聚易安",
+    "recommendDesc": "授信大额分期，随心所欲",
+    "applyUrl": "http://jyp.jjshanyin.com/#/proxyReg?inviteCode=A100248",
+    "code": "juyian"
   },
-  {
-    "joincount": "589238",
-    "imagepath": "dabaifenqi.png",
-    "name": "大白分期",
-    "recommendDesc": "拒就送，最高888元现金",
-    "applyUrl": "http://jsqb.kdqugou.com/newh5/web/html/landing-page4.html?source_tag=125",
-    "code": "dabaifenqi"
-  },
+
   {
     "joincount": "948920",
     "imagepath": "zhongjinguanjia.png",
@@ -43,52 +45,71 @@ export const data = [
     "applyUrl": "http://zj.qianyoulu.cn/index.php/home/login/r/sign/14ff20c515e85802f0379e81bd07470c",
     "code": "zhongjinguanjia"
   },
-  {
-    "joincount": "932890",
-    "imagepath": "tongyuansudai.png",
-    "name": "通源速贷",
-    "recommendDesc": "实名秒到，一键借款",
-    "applyUrl": "https://tongyuansudai.fanzhoutech.com/mobile/applyform?par=45968AD65BACA8AA8CE1B42A4071194C",
-    "code": "tongyuansudai"
-  },
-  {
-    "joincount": "2323421",
-    "imagepath": "jiujiujie.png",
-    "name": "久久借",
-    "recommendDesc": "还款灵活，审批迅速",
-    "applyUrl": "http://www.wanrendai.top/?befrom=tg66",
-    "code": "jiujiujie"
-  },
-  {
-    "joincount": "2123620",
-    "imagepath": "fubaba.png",
-    "name": "富爸爸闪贷",
-    "recommendDesc": "5分钟放款，通过率90%",
-    "applyUrl": "http://588.sourcecodefinance.com/origin/register?model%253D5%2526eid%253D204%2526c%253D246",
-    "code": "fubaba"
-  },
-  {
-    "joincount": "1723421",
-    "imagepath": "miaobaitiao.png",
-    "name": "秒白条",
-    "recommendDesc": "全自动审批，18秒到账",
-    "applyUrl": "https://m.miaobt.com/wap/w/login/tg1/channel/jieyoubang",
-    "code": "miaobaitiao"
-  },
-  {
-    "joincount": "690321",
-    "imagepath": "woaika.png",
-    "name": "我爱卡",
-    "recommendDesc": "万元额度，7天免息",
-    "applyUrl": "http://m.51credit.com/promote/wt20170918.html?tf=wt34",
-    "code": "woaika"
-  },
-  {
-    "joincount": "1238949",
-    "imagepath": "miaodaiwang.png",
-    "name": "秒贷王",
-    "recommendDesc": "芝麻分580，千元秒借领回家！",
-    "applyUrl": "http://p.creditxb.com/?h=TSmHGC",
-    "code": "miaodaiwang"
-  },
+  //
+  // {
+  //   "joincount": "1348734",
+  //   "imagepath": "yangcongxinyong.png",
+  //   "name": "洋葱信用",
+  //   "recommendDesc": "无抵押，高通过",
+  //   "applyUrl": "http://channel.yangcongjietiao.com/channel/view/register.html?channel=jieyoubang",
+  //   "code": "yangcongxinyong"
+  // },
+  // {
+  //   "joincount": "893234",
+  //   "imagepath": "jiyonghebao.png",
+  //   "name": "急用荷包",
+  //   "recommendDesc": "芝麻分580，无逾期秒下",
+  //   "applyUrl": "http://business.haodaihome.com/haodai/loanCompanyHomePage?configCode=da6af473-ec6c-4dc0-82ac-7ea7d8d09cd9&channelCode=jieyoubang&agentId=54564",
+  //   "code": "jiyonghebao"
+  // },
+  // {
+  //   "joincount": "589238",
+  //   "imagepath": "dabaifenqi.png",
+  //   "name": "大白分期",
+  //   "recommendDesc": "拒就送，最高888元现金",
+  //   "applyUrl": "http://jsqb.kdqugou.com/newh5/web/html/landing-page4.html?source_tag=125",
+  //   "code": "dabaifenqi"
+  // },
+  //
+  // {
+  //   "joincount": "932890",
+  //   "imagepath": "tongyuansudai.png",
+  //   "name": "通源速贷",
+  //   "recommendDesc": "实名秒到，一键借款",
+  //   "applyUrl": "https://tongyuansudai.fanzhoutech.com/mobile/applyform?par=45968AD65BACA8AA8CE1B42A4071194C",
+  //   "code": "tongyuansudai"
+  // },
+  // {
+  //   "joincount": "2323421",
+  //   "imagepath": "jiujiujie.png",
+  //   "name": "久久借",
+  //   "recommendDesc": "还款灵活，审批迅速",
+  //   "applyUrl": "http://www.wanrendai.top/?befrom=tg66",
+  //   "code": "jiujiujie"
+  // },
+  // {
+  //   "joincount": "2123620",
+  //   "imagepath": "fubaba.png",
+  //   "name": "富爸爸闪贷",
+  //   "recommendDesc": "5分钟放款，通过率90%",
+  //   "applyUrl": "http://588.sourcecodefinance.com/origin/register?model%253D5%2526eid%253D204%2526c%253D246",
+  //   "code": "fubaba"
+  // },
+  // {
+  //   "joincount": "1723421",
+  //   "imagepath": "miaobaitiao.png",
+  //   "name": "秒白条",
+  //   "recommendDesc": "全自动审批，18秒到账",
+  //   "applyUrl": "https://m.miaobt.com/wap/w/login/tg1/channel/jieyoubang",
+  //   "code": "miaobaitiao"
+  // },
+  //
+  // {
+  //   "joincount": "1238949",
+  //   "imagepath": "miaodaiwang.png",
+  //   "name": "秒贷王",
+  //   "recommendDesc": "芝麻分580，千元秒借领回家！",
+  //   "applyUrl": "http://p.creditxb.com/?h=TSmHGC",
+  //   "code": "miaodaiwang"
+  // },
 ];
