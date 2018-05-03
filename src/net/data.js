1ad8c1ec139ff204ad3e992f5ed94f02dbb64c77
pain.xie <pain.xie@wolaidai.com>
2018-05-03 23:00:47 +0800

    v1.0.0 合作方更新

--- src/net/data.js
+++ src/net/data.js
@@ -4,20 +4,20 @@
 
 export const data = [
   {
+    "joincount": "1365988",
+    "imagepath": "yaoqianshu.png",
+    "name": "摇钱树",
+    "recommendDesc": "芝麻分600，极速下款",
+    "applyUrl": "http://www.yqs.hzrongzhida.com/h5/register.html?agentId=422",
+    "code": "yaoqianshu"
+  },
+  {
     "joincount": "1589023",
     "imagepath": "weilixin.png",
     "name": "微粒信",
     "recommendDesc": "零门槛，极速借",
     "applyUrl": "http://app.welixin.net/index.php/home/login/r/sign/9b4d5e7039f2b4c0dc06b021aa5e0b2a",
     "code": "weilixin"
-  },
-  {
-    "joincount": "1256246",
-    "imagepath": "xiaohuajinrong.png",
-    "name": "小花金融",
-    "recommendDesc": "芝麻分550，贷款秒下",
-    "applyUrl": "http://app.worldjunchi.com/web/mobile/smsproduct/list?pn=xhjr-dl",
-    "code": "xiaohuajinrong"
   },
   {
     "joincount": "948920",
@@ -28,62 +28,120 @@ export const data = [
     "code": "zhongjinguanjia"
   },
   {
-    "joincount": "569832",
-    "imagepath": "kanglexiang.png",
-    "name": "康乐享",
-    "recommendDesc": "极速认证，最高两万",
-    "applyUrl": "http://lxbk.jjkswl.com/#/proxyReg?inviteCode=A100447",
-    "code": "kanglexiang"
+    "joincount": "596266",
+    "imagepath": "kuaizu.png",
+    "name": "快租",
+    "recommendDesc": "闪电放款，最快66秒",
+    "applyUrl": "http://kuaizu.zhongmengzn.com/static/download.html?show=1&agentid=1022832",
+    "code": "kuaizu"
   },
   {
-    "joincount": "1236952",
-    "imagepath": "zhenyidai.png",
-    "name": "臻易贷",
-    "recommendDesc": "日息万三，万元额度",
-    "applyUrl": "http://47.98.47.208/act/light-loan-xjx?invite_code=NTIxMjg=&user_from=32",
-    "code": "zhenyidai"
+    "joincount": "896255",
+    "imagepath": "jiyuqianbao.png",
+    "name": "机遇钱包",
+    "recommendDesc": "极速认证，5分钟放款",
+    "applyUrl": "https://csjyb.nbxfkj.com/Home/reg?code=cd84f878d8df4d67ba46e2d2a8949002",
+    "code": "jiyuqianbao"
   },
   {
-    "joincount": "1582343",
-    "imagepath": "kahuanwang.png",
-    "name": "卡还王",
-    "recommendDesc": "信用卡还款，轻松无压力",
-    "applyUrl": "http://www.kahuanwang.com/khw/c/r/Kmaolei_lianjie6",
-    "code": "kahuanwang"
+    "joincount": "596551",
+    "imagepath": "baicaiqianbao.png",
+    "name": "百彩钱包",
+    "recommendDesc": "还款灵活，极速审核",
+    "applyUrl": "https://12i.cn/pG5iQ1",
+    "code": "baicaiqianbao"
   },
   {
-    "joincount": "596325",
-    "imagepath": "zhongxianghua.png",
-    "name": "众享花",
-    "recommendDesc": "5分钟申请，100秒到账",
-    "applyUrl": "http://www.dalaomen.com/Enjoy/template/reg.html?&t=jybgs",
-    "code": "zhongxianghua"
+    "joincount": "1692544",
+    "imagepath": "xinyongrensheng.png",
+    "name": "信用人生",
+    "recommendDesc": "认证简单，通过率高",
+    "applyUrl": "http://xyrsapi1.fengyjf.com/h5/invite.jsp?invitationCode=null&channelCode=jieyoubang",
+    "code": "xinyongrensheng"
   },
   {
-    "joincount": "1579342",
-    "imagepath": "juyian.png",
-    "name": "聚易安",
-    "recommendDesc": "授信大额分期，随心所欲",
-    "applyUrl": "http://jyp.jjshanyin.com/#/proxyReg?inviteCode=A100248",
-    "code": "juyian"
-  },
-  {
-
-    "joincount": "896254",
-    "imagepath": "yimiaojie.png",
-    "name": "一秒借",
+    "joincount": "875456",
+    "imagepath": "weichuangjinrong.png",
+    "name": "微创金融",
     "recommendDesc": "全国无死角，放款快",
-    "applyUrl": "https://yimiaojie.geziqianbao.com/yimiaojie/inborrow/redirectindex?cid=268479551#&/yimiaojie/passport/register?cid=268479551",
-    "code": "yimiaojie"
+    "applyUrl": "http://suo.im/1zhlKq",
+    "code": "weichuangjinrong"
   },
   {
-    "joincount": "2645741",
-    "imagepath": "tengfenqi.png",
-    "name": "腾分期",
-    "recommendDesc": "灵活分期，极速审核",
-    "applyUrl": "http://activity.10winner.com/cai_h5/landPage.html?key=682105",
-    "code": "tengfenqi"
+    "joincount": "496554",
+    "imagepath": "xinshidai.png",
+    "name": "新时贷",
+    "recommendDesc": "额度灵活，提现快",
+    "applyUrl": "https://ylxd.yunlibeauty.com/ylxd/home?channelId=1283&cid=1335",
+    "code": "xinshidai"
   },
+  //
+  // {
+  //   "joincount": "1256246",
+  //   "imagepath": "xiaohuajinrong.png",
+  //   "name": "小花金融",
+  //   "recommendDesc": "芝麻分550，贷款秒下",
+  //   "applyUrl": "http://app.worldjunchi.com/web/mobile/smsproduct/list?pn=xhjr-dl",
+  //   "code": "xiaohuajinrong"
+  // },
+  //
+  // {
+  //   "joincount": "569832",
+  //   "imagepath": "kanglexiang.png",
+  //   "name": "康乐享",
+  //   "recommendDesc": "极速认证，最高两万",
+  //   "applyUrl": "http://lxbk.jjkswl.com/#/proxyReg?inviteCode=A100447",
+  //   "code": "kanglexiang"
+  // },
+  // {
+  //   "joincount": "1236952",
+  //   "imagepath": "zhenyidai.png",
+  //   "name": "臻易贷",
+  //   "recommendDesc": "日息万三，万元额度",
+  //   "applyUrl": "http://47.98.47.208/act/light-loan-xjx?invite_code=NTIxMjg=&user_from=32",
+  //   "code": "zhenyidai"
+  // },
+  // {
+  //   "joincount": "1582343",
+  //   "imagepath": "kahuanwang.png",
+  //   "name": "卡还王",
+  //   "recommendDesc": "信用卡还款，轻松无压力",
+  //   "applyUrl": "http://www.kahuanwang.com/khw/c/r/Kmaolei_lianjie6",
+  //   "code": "kahuanwang"
+  // },
+  // {
+  //   "joincount": "596325",
+  //   "imagepath": "zhongxianghua.png",
+  //   "name": "众享花",
+  //   "recommendDesc": "5分钟申请，100秒到账",
+  //   "applyUrl": "http://www.dalaomen.com/Enjoy/template/reg.html?&t=jybgs",
+  //   "code": "zhongxianghua"
+  // },
+  // {
+  //   "joincount": "1579342",
+  //   "imagepath": "juyian.png",
+  //   "name": "聚易安",
+  //   "recommendDesc": "授信大额分期，随心所欲",
+  //   "applyUrl": "http://jyp.jjshanyin.com/#/proxyReg?inviteCode=A100248",
+  //   "code": "juyian"
+  // },
+  // {
+  //
+  //   "joincount": "896254",
+  //   "imagepath": "yimiaojie.png",
+  //   "name": "一秒借",
+  //   "recommendDesc": "全国无死角，放款快",
+  //   "applyUrl": "https://yimiaojie.geziqianbao.com/yimiaojie/inborrow/redirectindex?cid=268479551#&/yimiaojie/passport/register?cid=268479551",
+  //   "code": "yimiaojie"
+  // },
+  // {
+  //   "joincount": "2645741",
+  //   "imagepath": "tengfenqi.png",
+  //   "name": "腾分期",
+  //   "recommendDesc": "灵活分期，极速审核",
+  //   "applyUrl": "http://activity.10winner.com/cai_h5/landPage.html?key=682105",
+  //   "code": "tengfenqi"
+  // },
 
 
   //
